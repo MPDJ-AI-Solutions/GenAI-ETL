01680cb6f50873fb13378ead222b2d5a7214b08c
Dave <dj300758@student.polsl.pl>
2024-08-09 17:14:29 +0200

    0.5.1 links adjusted

--- Utils/describe_data.py
+++ Utils/describe_data.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 def print_file_info(path):
-    data = pd.read_csv(path, sep=';')
+    data = pd.read_csv(path)
     data.info()
 
-print_file_info('./Data/Apartments/apartments_pl_2023_08.csv')
-print_file_info('./Data/GUS/RYNE_3783_CTAB_20240731215821.csv')
-print_file_info('./Data/Jobs/202309_soft_eng_jobs_pol.csv')
+print_file_info('./Data/apartments_pl_2023_08.csv')
+print_file_info('./Data/RYNE_3783_CTAB_20240731215821.csv')
+print_file_info('./Data/202309_soft_eng_jobs_pol.csv')
